d20977aac40df2965086ad127447a68e931dce8d
Rob Bart <rbart@cs.washington.edu>
2012-11-11 21:45:02 -0800

    renamed relexplorer to interfaceb, rewrote it to use the new interfaces

---
+++ src/main/scala/edu/washington/cs/knowitall/nlp/util/InterfaceB.scala
@@ -0,0 +1,52 @@
+package edu.washington.cs.knowitall.nlp.util
+
+import scopt.OptionParser
+import edu.washington.cs.knowitall.nlp.ChunkedSentence
+
+import edu.washington.cs.knowitall.browser.hadoop.scoobi.TypeContextAggregator
+
+import edu.washington.cs.knowitall.tool.postag.PostaggedToken
+
+import scala.collection.JavaConversions._
+
+import relontology.WordNetUtils
+import relontology.CleanUtils
+
+// performs the task of stephen's command line interface "B" -
+// which is to take as input a relation phrase and return
+// wordnet info such as synonyms, entailments, 
+
+object InterfaceB {
+
+  def main(args: Array[String]) {
+    
+    var relPhrase = "";
+    
+    val parser = new OptionParser("Interface B") {
+      arg("relPhrase", "relation phrase", { str => relPhrase = str })
+    }
+    
+    if (!parser.parse(args)) return
+    
+    val wnUtils = WordNetUtils.defaultInstance
+    val cleanUtilsHtl = CleanUtils.defaultHtl
+    val cleanUtilsTncf = CleanUtils.defaultTncf
+    
+    val relToken = new PostaggedToken("VB", relPhrase, 0)
+    
+    println("WN Synonyms:")
+    println(wnUtils.getWnSynonyms(relToken).mkString(", "))
+    println("WN Hypernyms:")
+    println(wnUtils.getWnHypernyms(relToken).mkString(", "))
+    println("WN Troponyms:")
+    println(wnUtils.getWnTroponyms(relToken).mkString(", "))
+    println("\nCLEAN HTL Entailments:")
+    cleanUtilsHtl.getCleanEntailments(relPhrase) foreach println;
+    println("\nCLEAN TNCF Entailments:")
+    cleanUtilsTncf.getCleanEntailments(relPhrase) foreach println;
+  }
+}
+
+
+
+
